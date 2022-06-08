1fdea7bbd3e6c16045c1c1eec659808809853c86
2eeYunHo <mmm01230@gmail.com>
2022-06-08 16:21:07 +0900

    commit

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -25,161 +25,23 @@ public class UserRepository {
 	}
 
 	public UserVo findByEmailAndPassword(UserVo vo) {
-		UserVo result = null;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			connection =  dataSource.getConnection();
-
-			// 3. SQL 준비
-			String sql =  "select no, name" 
-						+ " from user" 
-						+ " where email = ?" 
-						+ " and password = ?";
-			pstmt = connection.prepareStatement(sql);
-
-			// binding
-			pstmt.setString(1, vo.getEmail());
-			pstmt.setString(2, vo.getPassword());
-
-			// 5. SQL 실행
-			rs = pstmt.executeQuery();
-
-			// 6. 결과처리
-			if (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-
-			}
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+		return sqlSession.selectOne("user.findByEmailAndPassword",vo);
 	}
 
 
 
 	public UserVo findByNo(Long userNo) {
-		UserVo result = null;
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-
-		try {
-			conn =  dataSource.getConnection();
-
-			String sql =  "select no, name, email, gender" 
-						+ " from user" 
-						+ " where no=?";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setLong(1, userNo);
-
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				Long no = rs.getLong(1);
-				String name = rs.getString(2);
-				String email = rs.getString(3);
-				String gender = rs.getString(4);
-
-				result = new UserVo();
-				result.setNo(no);
-				result.setName(name);
-				result.setEmail(email);
-				result.setGender(gender);
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+		return sqlSession.selectOne("user.findByNo", userNo);
 	}
 
 	public boolean update(UserVo vo) {
-		boolean result = false;
-		Connection connection = null;
-		PreparedStatement pstmt = null;
-
-		try {
-			connection =  dataSource.getConnection();
-			if ("".equals(vo.getPassword())) {
-				String sql =  "update user" 
-							+ " set name = ? , gender = ?" 
-							+ "	where no = ?";
-
-				pstmt = connection.prepareStatement(sql);
-
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getGender());
-				pstmt.setLong(3, vo.getNo());
-			}
-
-			else {
-				String sql =  "update user" 
-							+ " set name = ? , password = ?, gender = ?" 
-							+ "	where no = ?";
-
-				pstmt = connection.prepareStatement(sql);
-
-				pstmt.setString(1, vo.getName());
-				pstmt.setString(2, vo.getPassword());
-				pstmt.setString(3, vo.getGender());
-				pstmt.setLong(4, vo.getNo());
-			}
-			int count = pstmt.executeUpdate();
-			result = count == 1;
-		} catch (SQLException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (connection != null) {
-					connection.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-
-		return result;
+//			if ("".equals(vo.getPassword())) {
+//				return sqlSession.selectOne("user.update1", vo) ==1
+//			}
+//			else {
+//				return sqlSession.selectOne("user.update2", vo) ==1
+//			} 동적쿼리를 지원해 주기때문에 상단의 이프 엘스 문을 한줄로 줄여줄수있다. user.xml의 choose when otherwise문을 이용한 동적쿼리문!!!
+		return sqlSession.update("user.update",vo)==1;
 
 	}
 }
